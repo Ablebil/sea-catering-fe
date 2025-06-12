496f0826505c275a231ea257bfbc7ef6f8830684
Ablebil <hafizhfaiq07@gmail.com>
2025-06-12 13:13:18 +0700

    refactor: change cursor type to cursor-pointer when hover submit button

--- src/components/TestimonialForm.tsx
+++ src/components/TestimonialForm.tsx
@@ -94,7 +94,7 @@ const TestimonialForm: React.FC<TestimonialFormProps> = ({ onSubmit }) => {
 
       <button
         type="submit"
-        className="bg-green-600 hover:bg-green-700 text-white font-semibold px-4 py-2 rounded"
+        className="bg-green-600 hover:bg-green-700 text-white font-semibold px-4 py-2 rounded cursor-pointer"
       >
         Submit Testimonial
       </button>
