92504c5ecb18a32e669aa0727ef16a2492aa4a03
Ablebil <hafizhfaiq07@gmail.com>
2025-06-28 10:18:20 +0700

    refactor: clean up unused request interceptor code

--- src/services/api.ts
+++ src/services/api.ts
@@ -45,10 +45,6 @@ class ApiService {
 
     this.axiosInstance.interceptors.request.use(
       (config) => {
-        // const token = getAccessToken();
-        // if (token) {
-        //   config.headers.Authorization = `Bearer ${token}`;
-        // }
         return config;
       },
       (error) => Promise.reject(error)
